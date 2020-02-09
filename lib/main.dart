5f3c2a564df51d737776efe6fcf36fd032de7add
tianpengsheng <davidtps@126.com>
2020-02-09 11:31:53 +0800

    主页面 BottomNavigationBar组件使用提交；

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_news/part1/AccountSettingsPage.dart';
-import 'package:flutter_news/part1/AllItemsPage.dart';
-import 'package:flutter_news/part1/FreeFlowNewsPage.dart';
-import 'package:flutter_news/part1/LevelPage.dart';
-import 'package:flutter_news/part1/RecentPage.dart';
-import 'package:flutter_news/part1/SmallVideoPage.dart';
-import 'package:flutter_news/part1/SquarePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,16 +11,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-      routes: {
-        '/recent': (context) => RecentPage(),
-        '/level': (context) => LevelPage(),
-        '/account': (context) => AccountSettingsPage(),
-        '/video': (context) => SmallVideoPage(),
-        '/square': (context) => SquarePage(),
-        '/freeflownews': (context) => FreeFlowNewsPage(),
-        '/allitems': (context) => AllItemsPage(),
-      },
+      home: MyHomePage(title: '首页'),
+//      routes: {
+//        '/recent': (context) => RecentPage(),
+//        '/level': (context) => LevelPage(),
+//        '/account': (context) => AccountSettingsPage(),
+//        '/video': (context) => SmallVideoPage(),
+//        '/square': (context) => SquarePage(),
+//        '/freeflownews': (context) => FreeFlowNewsPage(),
+//        '/allitems': (context) => AllItemsPage(),
+//      },
     );
   }
 }
@@ -42,13 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -57,54 +44,113 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/recent');
-              },
-              child: Text("最近浏览"),
+        child: Text("内容"),
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.start,
+//          children: <Widget>[
+//            RaisedButton(
+//              onPressed: () {
+//                Navigator.of(context).pushNamed('/recent');
+//              },
+//              child: Text("最近浏览"),
+//            ),
+//            RaisedButton(
+//              onPressed: () {
+//                Navigator.of(context).pushNamed('/level');
+//              },
+//              child: Text("等级列表"),
+//            ),
+//            RaisedButton(
+//              onPressed: () {
+//                Navigator.of(context).pushNamed('/account');
+//              },
+//              child: Text("账号设置"),
+//            ),
+//            RaisedButton(
+//              onPressed: () {
+//                Navigator.of(context).pushNamed('/video');
+//              },
+//              child: Text("小视频"),
+//            ),
+//            RaisedButton(
+//              onPressed: () {
+//                Navigator.of(context).pushNamed('/square');
+//              },
+//              child: Text("圈子"),
+//            ),
+//            RaisedButton(
+//              onPressed: () {
+//                Navigator.of(context).pushNamed('/freeflownews');
+//              },
+//              child: Text("免流量新闻"),
+//            ),
+//            RaisedButton(
+//              onPressed: () {
+//                Navigator.of(context).pushNamed('/allitems');
+//              },
+//              child: Text("全部栏目"),
+//            ),
+//          ],
+//        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            title: Text("首页"),
+            icon: Icon(
+              Icons.home,
+              size: 24,
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/level');
-              },
-              child: Text("等级列表"),
+//            backgroundColor: Colors.white,
+            activeIcon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("视频"),
+            icon: Icon(
+              Icons.videocam,
+              size: 24,
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/account');
-              },
-              child: Text("账号设置"),
+//            backgroundColor: Colors.grey,
+            activeIcon: Icon(Icons.videocam),
+          ),
+          BottomNavigationBarItem(
+            title: Text("圈子"),
+            icon: Icon(
+              Icons.my_location,
+              size: 24,
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/video');
-              },
-              child: Text("小视频"),
+//            backgroundColor: Colors.white,
+            activeIcon: Icon(Icons.my_location),
+          ),
+          BottomNavigationBarItem(
+            title: Text(
+              "我的",
+              style: TextStyle(fontSize: 14, color: Colors.black),
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/square');
-              },
-              child: Text("圈子"),
+            icon: Icon(
+              Icons.face,
+              size: 24,
             ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/freeflownews');
-              },
-              child: Text("免流量新闻"),
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/allitems');
-              },
-              child: Text("全部栏目"),
-            ),
-          ],
-        ),
+//            backgroundColor: Colors.white,
+            activeIcon: Icon(Icons.face),
+          ),
+        ],
+
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        fixedColor: Colors.red,
+        //选中的颜色
+        onTap: (index) {
+          print("点击了第$index");
+          changeIndex(index);
+        },
       ),
     );
+  }
+
+  void changeIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
